c6260e0b8075dfbc57b25bfd16cb90598994a8c4
André Luis <andrelugomes@gmail.com>
2015-09-13 20:38:17 -0300

    hash com valor default

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -9,17 +9,27 @@ describe Hash do
     #Jeito Novo
     @hash = {hello: 'world'}
   end
+
+  it "deve recuperar o valor pela chame ':hello'" do
+    expect(@hash[:hello]).to eq('world')
+  end
  
   it "deve retornar meu Hash" do
     hash = HashTests.new
 
-    #Printing
-    puts hash.hashDeNomes
-
     expect(hash.hashDeNomes.size).to eq(4)
   end
- 
-  it "deve recuperar o valor pela chame ':hello'" do
-    expect(@hash[:hello]).to eq('world')
+
+  it "deve criar um Hash vazio e com valor default" do
+  	#hash = Hash.new  -> {}
+
+  	hash = Hash.new('default')
+
+  	expect(hash.size).to eq(0)
+
+	hash[:primeiro]
+	hash[:segundo]
+  	#Atribuindo o primeiro valor ao Has
+  	expect(hash[:primeiro]).to eq("default")
   end
 end
